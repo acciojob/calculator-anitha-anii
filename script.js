03b52ee7a5b3b2ad7887ad461219abb91e79e4c9
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-05-24 19:12:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-//your code here
+let output = document.getElementById('input');
+let clearButton = document.getElementById('clear');
+
+clearButton.addEventListener('click', function() {
+  output.value ="";
+});
+// let output = document.getElementById('input');
+let allbuttons = document.querySelectorAll('button');
+// let clearbutton = document.getElementById('clear');
+
+
+
+for (let i = 0; i < allbuttons.length; i++) {
+  allbuttons[i].addEventListener('click', function() {
+    if (this.innerHTML === "=") {
+      try {
+        output.value = eval(output.value);
+      } catch (error) {
+        output.value = "Error";
+      }
+    } else if (this.innerHTML === "clear") {
+      output.value = "";
+    } else {
+      output.value += this.innerHTML;
+    }
+
+    // Handle division by zero and zero divided by zero
+    if (
+      this.innerHTML === "/" &&
+      (output.value.endsWith("/0") || output.value.endsWith("/ 0"))
+    ) {
+      output.value = "Infinity";
+    } else if (output.value === "0/0") {
+      output.value = "NaN";
+    }
+  });
+	
+
+}
+
+
